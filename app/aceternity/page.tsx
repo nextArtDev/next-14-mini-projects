144894381c57b14ad6b339803369e57c7973733b
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2024-01-27 00:07:48 +0330

    evervault card

--- app/aceternity/page.tsx
+++ app/aceternity/page.tsx
@@ -1,9 +1,11 @@
 import { GoogleGeminiEffectDemo } from './components/GoogleGeminiEffect/Demo'
+import { EvervaultCardDemo } from './components/evervault-card/Demo'
 
 function page() {
   return (
     <div>
       <GoogleGeminiEffectDemo />
+      <EvervaultCardDemo />
     </div>
   )
 }
