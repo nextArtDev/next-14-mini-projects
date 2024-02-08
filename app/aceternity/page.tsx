2f8602ab2eea6d4ef3c0e9d31966febe3ecdecac
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2024-02-08 21:34:50 +0330

    layout grid

--- app/aceternity/page.tsx
+++ app/aceternity/page.tsx
@@ -3,12 +3,14 @@ import { GoogleGeminiEffectDemo } from './components/GoogleGeminiEffect/Demo'
 import { TabsDemo } from './components/Tabs/Demo'
 import { EvervaultCardDemo } from './components/evervault-card/Demo'
 import { HeroParallaxDemo } from './components/hero-parallax/Demo'
+import { LayoutGridDemo } from './components/layout-grid/Demo'
 import { SVGMaskEffectDemo } from './components/mask-svg/Demo'
 import { TextRevealCardPreview } from './components/text-reveal/Demo'
 
 function page() {
   return (
     <div>
+      <LayoutGridDemo />
       <GoogleGeminiEffectDemo />
       <EvervaultCardDemo />
       <TabsDemo />
