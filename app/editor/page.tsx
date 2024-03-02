d24976405b4c1e635f2c3f84fedd5375abd4fe90
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2024-03-02 14:47:21 +0330

    plate ui

---
+++ app/editor/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { Editor } from '@/components/plate-ui/editor'
+import React from 'react'
+import { PlateEditor } from './Example'
+
+type Props = {}
+
+function page({}: Props) {
+  return <PlateEditor />
+}
+
+export default page
