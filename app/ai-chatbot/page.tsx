9e01387dcc2bd3f6bf922a7ae24bbf9b1108efbd
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2025-01-25 16:50:37 +0330

    chat demo

--- app/ai-chatbot/page.tsx
+++ app/ai-chatbot/page.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { ChatDemo } from './components/chat-demo'
+import { Card } from '@/components/ui/card'
 
 //pnpm install react-markdown remark-gfm shiki
 //npm install framer-motion@11 remeda@2
+//https://github.com/Blazity/shadcn-chatbot-kit/tree/main
+
 type Props = {}
 
 const page = (props: Props) => {
-  return <div>page</div>
+  return (
+    <div className="flex items-center justify-center min-h-screen max-w-sm mx-auto">
+      <Card className="p-3">
+        <ChatDemo />
+      </Card>
+    </div>
+  )
 }
 
 export default page
